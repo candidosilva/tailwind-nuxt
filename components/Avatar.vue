2b15e05ca8836be15ec936344eac7f4fb3882b51
Izabella Gomes de Souza <57716228+candidosilva@users.noreply.github.com>
2023-07-06 11:30:34 -0300

    Added avatar component
    Added avatar component

---
+++ components/Avatar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="relative">
+    <nuxt-img
+      v-if="img"
+      class="h-10 w-10"
+      :class="[squareAvatar, border]"
+      :src="img"
+      :alt="alt"
+    />
+
+    <div
+      v-else-if="initials"
+      class="flex h-10 w-10 items-center justify-center bg-gray-100 text-xl font-medium text-gray-600"
+      :class="[squareAvatar, border]"
+    >
+      {{ initials }}
+    </div>
+
+    <div
+      v-else
+      class="h-10 w-10 rounded-full bg-gray-100"
+      :class="[squareAvatar, border]"
+    >
+      <Icon name="mdi:account" size="40" color="gray" />
+    </div>
+
+    <span
+      v-if="dot"
+      class="absolute h-3.5 w-3.5 rounded-full border-2 border-white bg-red-400"
+      :class="[square ? '-top-2 left-8' : 'left-7 top-0']"
+    ></span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  img: {
+    type: String,
+  },
+  alt: {
+    type: String,
+  },
+  square: {
+    type: Boolean,
+  },
+  border: {
+    type: Boolean,
+  },
+  initials: {
+    type: String,
+  },
+  dot: {
+    type: Boolean,
+  },
+});
+
+const squareAvatar = computed(() => {
+  if (props.square) return "rounded";
+  return "rounded-full";
+});
+
+const border = computed(() => {
+  if (props.border) return "ring-2 ring-blue-500";
+  return "";
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <button
-    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
-  >
-    teste
-  </button>
+  <div class="m-2 flex flex-col items-center justify-center gap-2">
+    <p>defult</p>
+    <Avatar img="woman.jpg" />
+
+    <p>square</p>
+    <Avatar img="woman.jpg" square />
+
+    <p>border</p>
+    <Avatar img="woman.jpg" border />
+
+    <p>border square</p>
+    <Avatar img="woman.jpg" square border />
+
+    <p>icon</p>
+    <Avatar />
+
+    <p>initials</p>
+    <Avatar initials="CS" />
+
+    <p>dot</p>
+    <Avatar img="woman.jpg" dot />
+  </div>
 </template>
+
+<script setup>
+const alert = ref("Simple text information.");
+</script>
